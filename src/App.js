f69117dcf2646a1f30b92d22afee6d28a59fcb36
Joel Taylor <jtayped@gmail.com>
2023-01-20 19:48:44 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
-import { Navbar, Hero, Macbook } from "./containers/index";
+import { Navbar, Hero } from "./containers/index";
 import { HeroItem } from "./components/index";
 import MacBookHeroImage from "./assets/hero-images/MacBook.jpg";
 import MacMini from "./assets/hero-images/MacMini.jpg";
+import HomePod from "./assets/hero-images/HomePod.jpg";
 
 function App() {
   const heroItems = [
@@ -16,13 +17,22 @@ function App() {
       orderNowLink: "https://www.apple.com/us/shop/goto/buy_mac/macbook_pro_14",
     },
     {
-      id: 1,
+      id: 2,
       title: "Mac mini",
       subtitle: "Supercharged by M2 and M2 Pro.",
       version: "1.24",
       image: MacMini,
       learnMoreLink: "https://www.apple.com/macbook-pro-14-and-16/",
       orderNowLink: "https://www.apple.com/us/shop/goto/buy_mac/macbook_pro_14",
+    },
+    {
+      id: 3,
+      title: "HomePod",
+      subtitle: "Profound sound.",
+      version: "2.3",
+      image: HomePod,
+      learnMoreLink: "https://www.apple.com/homepod-2nd-generation/",
+      orderNowLink: "https://www.apple.com/us/shop/goto/buy_homepod/homepod",
     },
   ];
   return (
